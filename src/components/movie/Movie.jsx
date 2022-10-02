4037ad4c80d75be8f05cb0f7865c18909a281b76
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-10-02 21:17:21 +0200

    Estructura base

---
+++ src/components/movie/Movie.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Movie (title)  {
+  return (
+    <div>
+        <h2 key={movie.id}>{movie.title}</h2>
+        <h2>test</h2>
+        <h2>{title}</h2>
+        {/* <img src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path}/> */}
+    </div>
+  )
+}
+
+export default Movie
