80e647a6bb60291127281d06761af643dd1af313
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-10-23 22:16:47 +0200

    cambios en el overview

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -12,13 +12,23 @@ function Movie({ movie }) {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div>
+      <div className="container_movie">
         <img
           className="img_movie"
           src={apiConfig.w500Image + movie.backdrop_path}
           alt=""
         />
-        <h2 className="title_movie">{movie.title}</h2>
+        <span className="shadow"></span>
+        <div className="cualification">
+          <h2 className="title_movie">{movie.title}</h2>
+        </div>
+        <div className="container_overview">
+          <div className="data_overview">
+            <h2 className="title_overview">{movie.title}</h2>
+            <h3 className={movie.vote_average<7? "popularity_movie": "popularity_movie green"  }>{movie.vote_average}</h3>
+          </div>
+          <p className="overview">{movie.overview}</p>
+        </div>
       </div>
     </motion.div>
   );
